f1eacae670acef2f6beff6a9c64d4c79e733d561
alison <alison.alves@academico.ifpb.edu.br>
2025-10-28 15:13:16 -0300

    feat: Ajusta MainPapelSave para usar enum NomePapel e corrige exceções

--- src/main/java/br/edu/ifpb/es/daw/MainPapelSave.java
+++ src/main/java/br/edu/ifpb/es/daw/MainPapelSave.java
@@ -2,27 +2,31 @@ package br.edu.ifpb.es.daw;
 
 import br.edu.ifpb.es.daw.dao.PapelDAO;
 import br.edu.ifpb.es.daw.dao.impl.PapelDAOImpl;
+import br.edu.ifpb.es.daw.dao.PersistenciaDawException;
+import br.edu.ifpb.es.daw.entities.NomePapel; // Importe o enum NomePapel
 import br.edu.ifpb.es.daw.entities.Papel;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
 public class MainPapelSave {
 
-    public static void main(String[] args) throws DawException {
+    public static void main(String[] args) throws PersistenciaDawException {
         try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
             PapelDAO dao = new PapelDAOImpl(emf);
 
             System.out.println("Salvando novos papéis...");
 
+            // --- Salvar Papel Cliente ---
             Papel papelCliente = new Papel();
-
-            papelCliente.setNome("ROLE_CLIENTE_" + System.nanoTime());
+            // CORREÇÃO: Usando NomePapel.CLIENTE (sem o "ROLE_")
+            papelCliente.setNome(NomePapel.CLIENTE);
             dao.save(papelCliente);
             System.out.println("Papel salvo com sucesso! " + papelCliente);
 
+            // --- Salvar Papel Admin ---
             Papel papelAdmin = new Papel();
-
-            papelAdmin.setNome("ROLE_ADMIN_" + System.nanoTime());
+            // CORREÇÃO: Usando NomePapel.ADMIN (sem o "ROLE_")
+            papelAdmin.setNome(NomePapel.ADMIN);
             dao.save(papelAdmin);
             System.out.println("Papel salvo com sucesso! " + papelAdmin);
         }
--- src/main/java/br/edu/ifpb/es/daw/MainPedidoSave.java
+++ src/main/java/br/edu/ifpb/es/daw/MainPedidoSave.java
@@ -1,8 +1,11 @@
+// src/main/java/br/edu/ifpb/es/daw/MainPedidoSave.java
 package br.edu.ifpb.es.daw;
 
 import br.edu.ifpb.es.daw.dao.PedidoDAO;
 import br.edu.ifpb.es.daw.dao.impl.PedidoDAOImpl;
+import br.edu.ifpb.es.daw.dao.PersistenciaDawException; // Importação correta da exceção
 import br.edu.ifpb.es.daw.entities.Pedido;
+import br.edu.ifpb.es.daw.entities.StatusPedido;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
@@ -10,15 +13,15 @@ import java.time.LocalDateTime;
 
 public class MainPedidoSave {
 
-    public static void main(String[] args) throws DawException {
+    // AQUI: Adicione 'throws PersistenciaDawException'
+    public static void main(String[] args) throws PersistenciaDawException {
         try(EntityManagerFactory emf = Persistence.createEntityManagerFactory("daw")) {
             PedidoDAO dao = new PedidoDAOImpl(emf);
 
             System.out.println("Salvando novo pedido...");
             Pedido pedido = new Pedido();
             pedido.setDataDoPedido(LocalDateTime.now());
-            pedido.setStatus("AGUARDANDO_PAGAMENTO");
-            pedido.setStatus("AGUARDANDO_PAGAMENTO_" + System.nanoTime());
+            pedido.setStatus(StatusPedido.ENVIADO); // Ou ENTREGUE, CANCELADO
             pedido.setValorTotal(199.99);
 
             dao.save(pedido);
