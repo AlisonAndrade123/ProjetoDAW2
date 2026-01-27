8a89d786712dd9b147d0390a4c2d7bb4c80b9555
DarioArthur <darioarthur44@gmail.com>
2026-01-27 13:36:37 -0300

    atualizando Produto

--- src/main/java/br/edu/ifpb/es/daw/controller/ProdutoController.java
+++ src/main/java/br/edu/ifpb/es/daw/controller/ProdutoController.java
@@ -5,7 +5,9 @@ import br.edu.ifpb.es.daw.dao.ProdutoDAO;
 import br.edu.ifpb.es.daw.dao.impl.ProdutoDAOImpl;
 import br.edu.ifpb.es.daw.entities.Produto;
 import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.PersistenceException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,13 +20,14 @@ public class ProdutoController {
     @Autowired
     private EntityManagerFactory emf;
 
+    // GET ALL
     @GetMapping
     public List<Produto> listar() throws PersistenciaDawException {
         ProdutoDAO dao = new ProdutoDAOImpl(emf);
         return dao.getAll();
     }
 
-    // 🔴 ESTE MÉTODO FALTAVA
+    // GET BY ID
     @GetMapping("/{id}")
     public ResponseEntity<Produto> buscarPorId(@PathVariable Long id)
             throws PersistenciaDawException {
@@ -32,8 +35,62 @@ public class ProdutoController {
         ProdutoDAO dao = new ProdutoDAOImpl(emf);
         Produto produto = dao.getByID(id);
 
-        return (produto != null)
+        return produto != null
                 ? ResponseEntity.ok(produto)
                 : ResponseEntity.notFound().build();
+    }
+
+    // POST
+    @PostMapping
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto)
+            throws PersistenciaDawException {
+
+        ProdutoDAO dao = new ProdutoDAOImpl(emf);
+
+        try {
+            dao.save(produto);
+            return ResponseEntity.status(HttpStatus.CREATED).body(produto);
+        } catch (PersistenceException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
+    }
+
+    // PUT
+    @PutMapping("/{id}")
+    public ResponseEntity<Produto> atualizar(
+            @PathVariable Long id,
+            @RequestBody Produto produto) throws PersistenciaDawException {
+
+        ProdutoDAO dao = new ProdutoDAOImpl(emf);
+        Produto existente = dao.getByID(id);
+
+        if (existente == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        produto.setId(id);
+        dao.update(produto);
+
+        return ResponseEntity.ok(produto);
+    }
+
+    // DELETE
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id)
+            throws PersistenciaDawException {
+
+        ProdutoDAO dao = new ProdutoDAOImpl(emf);
+        Produto existente = dao.getByID(id);
+
+        if (existente == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        try {
+            dao.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (PersistenceException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 }
